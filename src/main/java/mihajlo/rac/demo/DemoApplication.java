76f606b56348a6b1067e71dce386a1b07b8226ff
mihajlo <k0kicE431440>
2017-12-16 18:28:46 +0100

    in progress..

--- src/main/java/mihajlo/rac/demo/DemoApplication.java
+++ src/main/java/mihajlo/rac/demo/DemoApplication.java
@@ -1,12 +1,26 @@
 package mihajlo.rac.demo;
 
+import mihajlo.rac.demo.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoApplication {
+import javax.annotation.Resource;
+
+
+public class DemoApplication implements CommandLineRunner {
+
+	@Resource
+	StorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
+	}
 }
+
