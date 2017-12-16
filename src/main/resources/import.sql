79b8947ba39437546ca06f8fbf41532cdcbb291d
SimicIlija <simicsilija@gmail.com>
2017-12-16 21:26:39 +0100

    import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
 INSERT INTO korisnik (ime, email, password,) VALUES ('admin', 'admin@admin', 'admin');
 INSERT INTO korisnik (ime, email, password,) VALUES ('a', 'a@a.com', 'a');
+INSERT INTO korisnik (ime, email, password,) VALUES ('po', 'po', 'po');
